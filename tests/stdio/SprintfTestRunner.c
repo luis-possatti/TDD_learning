a339053d9cc8141825c103686655212731674d92
Luis Henrique <luis.possatti@gmail.com>
2021-07-10 15:15:53 -0300

    very long string case

--- tests/stdio/SprintfTestRunner.c
+++ tests/stdio/SprintfTestRunner.c
@@ -30,6 +30,7 @@ TEST_GROUP_RUNNER(sprintf)
 {
     RUN_TEST_CASE(sprintf, NoFormatOperations);
     RUN_TEST_CASE(sprintf, InsertString);
+    RUN_TEST_CASE(sprintf, VeryBigString);
 #if 0
     RUN_TEST_CASE(sprintf, StringWithSpace);
 #endif
