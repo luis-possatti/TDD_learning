c615e05efa8bad8225411f327363a3b2895afcf8
Luis Henrique <luis.possatti@gmail.com>
2021-08-28 15:01:12 -0300

    Refactoring code
    Creating helper function to convert led index to respective bit

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -1,7 +1,21 @@
 #include "LedDriver.h"
 
+/* internal variables */
 static uint16_t *led_address_register;
 
+
+/* Internal function prototypes and definitions*/
+
+static uint16_t convert_led_index_to_bit(uint8_t index);
+
+static uint16_t convert_led_index_to_bit(uint8_t index)
+{
+    return (uint16_t) (1 << (index - 1));
+}
+
+
+
+/* Public functions definitions */
 void LedDriver_Creator(uint16_t *led_address)
 {
     led_address_register = led_address;
@@ -11,10 +25,12 @@ void LedDriver_Creator(uint16_t *led_address)
 
 void LedDriver_TurnOn(uint8_t led_index)
 {
-    *led_address_register |= (1 << (led_index - 1));   
+    *led_address_register |= convert_led_index_to_bit(led_index);   
 }
 
 void LedDriver_TurnOff(uint8_t led_index)
 {
-    *led_address_register &= ~(1 << (led_index - 1));
+    *led_address_register &= ~convert_led_index_to_bit(led_index);
 }
+
+
