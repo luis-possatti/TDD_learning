4064baf544651d5295c01c28d075e1e9b9902769
Luis Henrique <luis.possatti@gmail.com>
2021-09-07 11:16:55 -0300

    Start first tests related to the buffer fullness

--- tests/CircularBuffer/CircularBufferFullnessTest.c
+++ tests/CircularBuffer/CircularBufferFullnessTest.c
@@ -25,3 +25,25 @@ TEST(CircularBufferFull, BufferNotFullAfterCreation)
 {
     TEST_ASSERT_FALSE(CircularBuffer_IsFull(buffer_p));
 }
+
+/*
+ * Create, write N with N = $Size: full only after last write
+ * Ensure buffer is only full after writting same number of data
+ * than its size.
+ * 
+ */
+TEST(CircularBufferFull, BufferNotFullWhenWritingLessDataThanItsSize)
+{
+    uint8_t number_of_data = circular_buffer_size;
+
+    /* Write all the data except the last one*/
+    int i;
+    for(i = 0; i < number_of_data - 1; i++)
+    {
+        CircularBuffer_WriteValue(buffer_p, 0);
+        TEST_ASSERT_FALSE(CircularBuffer_IsFull(buffer_p));
+    }
+
+    CircularBuffer_WriteValue(buffer_p, 0);
+    TEST_ASSERT_TRUE(CircularBuffer_IsFull(buffer_p));
+}
